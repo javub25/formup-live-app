470f7bbd33d7d5bfded6ecb34e373864fe207b3d
javub25 <ju231415sad@gmail.com>
2024-11-13 11:41:28 +0100

    feat: add DndContext to FieldLibrary and FormBuilder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { FieldLibrary } from "@/components/FieldLibrary/FieldLibrary"
+import { FormBuilder } from "@/components/FormBuilder/FormBuilder"
+import { DndContext} from "@dnd-kit/core"
+import { handleDragStart, handleDragOver, handleDragEnd } from "@/utils/DragEvents/handleEvents"
+
 function App() {
 
   return (
     <main>
-        <FieldLibrary />
+      {/*Context to share data between Draggable (FielLibrary) and Droppable (FormBuilder)*/}
+        <DndContext 
+              onDragStart={handleDragStart}
+              onDragOver={handleDragOver}
+              onDragEnd={handleDragEnd}>
+              <FieldLibrary />
+              <FormBuilder />
+        </DndContext>
     </main>
   )
 }
-
 export default App
