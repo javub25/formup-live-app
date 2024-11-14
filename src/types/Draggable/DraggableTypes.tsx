af2a8b378890989430018ef0ebb2c8a09fcfe82c
javub25 <ju231415sad@gmail.com>
2024-11-14 20:21:44 +0100

    feat: add Draggable types

---
+++ src/types/Draggable/DraggableTypes.tsx
@@ -0,0 +1,11 @@
+
+export interface DescriptionType  {
+    attributes: {
+        "aria-roledescription": string;
+    }
+    description: string;
+}
+
+export interface DraggableType {
+    DraggableID: string
+}
