7020f48bddb6201a327c08604a411d39a4bdb668
javub25 <ju231415sad@gmail.com>
2024-11-21 16:37:16 +0100

    feat: add params to DndContext events

--- src/types/DndContext/EventsType.tsx
+++ src/types/DndContext/EventsType.tsx
@@ -1,8 +1,16 @@
 import { DragOverEvent, DragStartEvent, DragEndEvent } from "@dnd-kit/core"
 
 export type EventsType = {
-    handleDragStart: (event: DragStartEvent) => void;
-    handleDragOver: (event: DragOverEvent) => void;
-    handleDragEnd: (event: DragEndEvent) => void;
+    handleDragStart: (
+        event: DragStartEvent, 
+        setActiveDraggable: (currentDraggable : {DraggableID: string}) => void
+    ) => void;
+
+    handleDragOver: (event: DragOverEvent, 
+        setIsOverDroppable: (IsOver: boolean) => void
+    ) => void;
+    handleDragEnd: (event: DragEndEvent, 
+        setIsOverDroppable: (IsOver: boolean) => void
+    ) => void;
 }
 
