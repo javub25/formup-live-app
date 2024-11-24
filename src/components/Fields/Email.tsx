73785f23eb9ab75e3936293a862ee728131b5c72
javub25 <ju231415sad@gmail.com>
2024-11-24 13:27:37 +0100

    refactor(draggable): remove unused custom hook and utilities

--- src/components/Fields/Email.tsx
+++ src/components/Fields/Email.tsx
@@ -1,19 +1,9 @@
 import {Input, Label} from "@/utils/ShadcnElements.tsx"
-import { useCreateDraggable } from "@/hooks/CreateDraggable/useCreateDraggable"
-import { updateDescription } from "@/utils/Draggable/Attributes/updateDescription";
-import { getUniqueID } from "@/utils/DragDropID/getUniqueID";
 
 export const Email = () => 
-{
-    const UUID = getUniqueID();
-
-    const {attributes, listeners, style} = useCreateDraggable({DraggableID: `email-${UUID}`});
-    
-    updateDescription({attributes, description: "draggable email"});
-    
+{        
     return (
-        <div className="grid w-full max-w-sm items-center gap-1.5" 
-            {...attributes} {...listeners} style={style}>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="email" className="mb-2">Email</Label>
             <Input type="email" id="email" placeholder="Email" />
         </div>        
--- src/components/Fields/MultipleChoice.tsx
+++ src/components/Fields/MultipleChoice.tsx
@@ -1,8 +1,4 @@
 import { Checkbox, Label } from "@/utils/ShadcnElements.tsx";
-import { useCreateDraggable } from "@/hooks/CreateDraggable/useCreateDraggable"
-import { updateDescription } from "@/utils/Draggable/Attributes/updateDescription";
-import { getUniqueID } from "@/utils/DragDropID/getUniqueID";
-
 
 export const MultipleChoice = () => {
     const items = [
@@ -23,14 +19,9 @@ export const MultipleChoice = () => {
             label: "Option 4",
         },
     ];
-    const UUID = getUniqueID();
-
-    const {attributes, listeners, style} = useCreateDraggable({DraggableID: `MultipleChoice-${UUID}`})
-
-    updateDescription({attributes, description: "draggable MultipleChoice"});
 
     return (
-        <div className="grid w-full max-w-sm items-center gap-1.5" {...attributes} {...listeners} style={style}>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
             {items.map((item) => (
                 <div key={item.id} className="flex items-center space-x-2">
                     <Checkbox id={item.id} />
