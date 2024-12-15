480ef40fd80676212bc0ebf81d8fe7e98c2ee4be
javub25 <ju231415sad@gmail.com>
2024-12-15 19:11:07 +0100

    refactor(Droppable): delete custom hook DroppableItems and related methods

--- src/utils/Droppable/removeItems.tsx
+++
@@ -1,6 +0,0 @@
-import { removeItemsType } from "@/types/Droppable/DroppableType.tsx";
-
-export const removeItems = (params: removeItemsType) => {
-    const {id, setDroppableItems} = params;
-    setDroppableItems(oldDroppable => oldDroppable.filter(item => item.id !== id));
-}
