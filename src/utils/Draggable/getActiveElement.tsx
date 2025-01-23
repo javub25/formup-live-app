fc8d0574332dbaa88b39bf8911797a5d730a6e38
javub25 <ju231415sad@gmail.com>
2025-01-23 11:51:33 +0100

    refactor: add data folder to models

--- src/utils/Draggable/getActiveElement.tsx
+++ src/utils/Draggable/getActiveElement.tsx
@@ -1,4 +1,4 @@
-import { DraggableList } from "@/models/DraggableList";
+import { DraggableList } from "@/data/models/DraggableList";
 import { DraggableType } from "@/types/Draggable/DraggableTypes";
 
 export const getActiveElement = (id: DraggableType["id"]) => DraggableList.find((draggable) => draggable.id === id)
