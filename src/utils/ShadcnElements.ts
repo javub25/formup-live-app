d0cea736ede67bdcbfc8f02adebb29e5839c9bb0
javub25 <ju231415sad@gmail.com>
2025-01-23 12:41:13 +0100

    chore: delete react-checkbox and radio-group

--- src/utils/ShadcnElements.ts
+++ src/utils/ShadcnElements.ts
@@ -1,7 +1,5 @@
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Checkbox } from "@/components/ui/checkbox"
 import {Sheet, SheetContent, SheetTrigger, SheetHeader, SheetTitle, SheetDescription} from "@/components/ui/sheet"
 
-export {Input, Label, RadioGroup, RadioGroupItem, Checkbox, Sheet, SheetContent, SheetTrigger, SheetHeader, SheetTitle, SheetDescription};
+export {Input, Label, Sheet, SheetContent, SheetTrigger, SheetHeader, SheetTitle, SheetDescription};
